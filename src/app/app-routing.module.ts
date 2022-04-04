6a8464aa8f67d220b0c8e960ee52674d71fcd439
msadeq12 <msadeq27@yahoo.com>
2022-04-04 09:18:59 -0400

    created the initial components to work with

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {CourseInsertComponent} from "./course-insert/course-insert.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path: 'addCourse', component: CourseInsertComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { NavbarComponent } from './navbar/navbar.component';
+import { CourseInsertComponent } from './course-insert/course-insert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CourseInsertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
