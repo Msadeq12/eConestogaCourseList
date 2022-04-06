c7df1aed456ac774595ee8a89567a9dbf2705253
msadeq12 <msadeq27@yahoo.com>
2022-04-06 13:28:43 -0400

    created the initial components to work with x2

---
+++ src/app/course-insert/course-insert.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseInsertComponent } from './course-insert.component';
+
+describe('CourseInsertComponent', () => {
+  let component: CourseInsertComponent;
+  let fixture: ComponentFixture<CourseInsertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CourseInsertComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CourseInsertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/course-insert/course-insert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-insert',
+  templateUrl: './course-insert.component.html',
+  styleUrls: ['./course-insert.component.css']
+})
+export class CourseInsertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
